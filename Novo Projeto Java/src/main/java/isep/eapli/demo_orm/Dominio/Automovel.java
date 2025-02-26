5b29efa8ba66fb290c0e0daf9db8022f199fac9c
João Soares <1211064@isep.ipp.pt>
2025-02-26 16:58:50 +0000

    Implementação do método toString da classe GrupoAutomovel.

--- Novo Projeto Java/src/main/java/isep/eapli/demo_orm/Dominio/Automovel.java
+++ Novo Projeto Java/src/main/java/isep/eapli/demo_orm/Dominio/Automovel.java
@@ -5,11 +5,12 @@ import java.util.Date;
 public class Automovel {
 
     private String matricula;
-    private int AnoDeFabrico;
-    private int AnoDeAquisicao;
+    private int anoDeFabrico;
+    private int anoDeAquisicao;
     private String cor;
     private String cilindrada;
     private GrupoAutomovel grupoAutomovel;
+
 
 
 }
--- Novo Projeto Java/src/main/java/isep/eapli/demo_orm/Dominio/GrupoAutomovel.java
+++ Novo Projeto Java/src/main/java/isep/eapli/demo_orm/Dominio/GrupoAutomovel.java
@@ -15,4 +15,8 @@ public class GrupoAutomovel {
         this.classe=classe;
     }
 
+    @Override
+    public String toString() {
+        return String.format("Grupo %s, %d Portas, %f€, Classe %s",nomeDoGrupo,portas,preco,classe);
+    }
 }
