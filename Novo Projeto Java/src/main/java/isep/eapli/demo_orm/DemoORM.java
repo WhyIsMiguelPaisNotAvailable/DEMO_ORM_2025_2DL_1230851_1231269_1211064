2f20bd80c81e0a1cb4951a416c0484daaf8c03fe
João Soares <1211064@isep.ipp.pt>
2025-03-12 19:05:49 +0000

    Projeto novo

--- Novo Projeto Java/src/main/java/isep/eapli/demo_orm/DemoORM.java
+++
@@ -1,9 +0,0 @@
-package isep.eapli.demo_orm;
-
-import isep.eapli.demo_orm.apresentacao.MainMenu;
-
-public class DemoORM {
-    public static void main(String[] args) {
-        MainMenu.mainLoop();
-    }
-}
